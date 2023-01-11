457ec870c6726a616c5fa15cd5d87e06204687e7
Siham TAHI <s.tahi@reseau-canopee.fr>
2023-01-11 12:02:15 +0100

    chore : initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+
+/*This is a file configuration of Lint
+  extends : contains rules from other configurations rules.
+  overrides : we can special ruls from a specific file 
+
+
+*/
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier'
+  ],
+  overrides: [
+    {
+      files: [
+        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
+      ],
+      'extends': [
+        'plugin:cypress/recommended'
+      ]
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
