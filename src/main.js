26c355656fa9ff8a1ddfbeb6405eddf14f55678f
TAHI Siham <gs_tahi@esi.dz>
2023-01-27 17:50:01 +0100

    feat: form validation rules

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import veeValidatePlugin from "./includes/validation";
 
 import "./assets/main.css";
 import "./assets/base.css";
@@ -11,5 +12,6 @@ const app = createApp(App);
 // the use function allow us to register a plugin
 app.use(createPinia()); // THIS
 app.use(router);
+app.use(veeValidatePlugin);
 
 app.mount("#app");
