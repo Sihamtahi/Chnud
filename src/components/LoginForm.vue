2e46715aa4be9cc257d4b82ba8d5a77bf98cdd74
TAHI Siham <gs_tahi@esi.dz>
2023-02-20 15:53:42 +0100

    feat: define and add login state variables for loginForm vue

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,4 +1,11 @@
 <template>
+  <div
+    class="text-white text-center font-bold p-4 mb-4"
+    v-if="login_show_alert"
+    :class="login_alert_variant"
+  >
+    {{ login_alert_message }}
+  </div>
   <vee-form :validation-schema="loginSchema" @submit="login">
     <!-- Email -->
     <div class="mb-3">
@@ -25,6 +32,7 @@
     </div>
     <button
       type="submit"
+      :disabled="login_in_submission"
       class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
     >
       Submit
@@ -41,10 +49,21 @@ export default {
         email: "required|min:3|max:100|email",
         password: "required|min:8|max:100",
       },
+      login_in_submission: false, //disable the button while the form is submetting the data
+      login_show_alert: false, //toggle the alert visibility
+      login_alert_variant: "bg-blue-500",
+      login_alert_message: "Please wait ! We are logging you in.", // the message inside the alert
     };
   },
   methods: {
     login(values) {
+      this.login_in_submission = true;
+      this.login_show_alert = true;
+      this.login_alert_variant = "bg-blue-500";
+      this.login_alert_message = "Please wait ! We are logging you in.";
+      this.login_alert_variant = "bg-green-500";
+      this.login_alert_message = "Sucess ! You are now Logged in";
+
       console.log(values);
     },
   },
