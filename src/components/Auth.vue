4d98402f922a0ac8582bfc65ef21c5825c08cd2a
TAHI Siham <gs_tahi@esi.dz>
2023-02-20 11:59:10 +0100

    feat: add default select iof the country

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -93,6 +93,7 @@
             v-show="tab === 'register'"
             :validation-schema="schema"
             @submit="register"
+            :initial-values="userData"
           >
             <!-- Name -->
             <div class="mb-3">
@@ -213,6 +214,9 @@ export default {
         confirm_password: "confirmed:@password",
         country: "required|not_one_of:Antarctica",
         tos: "required",
+      },
+      userData: {
+        country: "USA",
       },
     };
   },
