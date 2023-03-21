077dbbac563665b808a1dc3668a97280f8f207f0
TAHI Siham <gs_tahi@esi.dz>
2023-03-21 15:42:41 +0100

    feat: add routing links for the header and manage page

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,29 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
+import Manage from "@/views/Manage.vue";
 const routes = [
   {
+    name: "home",
     path: "/",
     component: Home,
   },
   {
+    name: "about",
     path: "/about",
     component: About,
+  },
+  {
+    name: "manage",
+    path: "/manage",
+    component: Manage,
   },
 ];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   //history API to handle the navigation inside the docuement.
   routes,
+  linkActiveClass: "text-yellow-500",
 });
 
 export default router;
